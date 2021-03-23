24462a4dfff639e46da37b6fbc842995b1b921d9
= <nick@innomadic.com>
2021-03-23 10:32:22 +0300

    Added bootstrap component examples

--- app/routes.py
+++ app/routes.py
@@ -4,5 +4,13 @@ from app import app
 
 @app.route('/')
 def hello():
-    flavors = ["vanilla", "chocolate"]
-    return render_template('base.html', title='Home', user='nick', flavors=flavors)
+    return render_template('base.html', title='Home', user='nick')
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title='About Us')
+
+@app.route('/flavors')
+def flavors():
+    flavors = ["strawberry", "mango", "chocolate"]
+    return render_template('flavors.html', title='Flavors', user='nick', flavors=flavors)
